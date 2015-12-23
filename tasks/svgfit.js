011ec01a8b07c49e27b4ea53b49522f8191a8944
mikemellor11 <mikemellor11@hotmail.com>
2015-12-23 10:50:58 +0000

    Async calls

--- tasks/svgfit.js
+++ tasks/svgfit.js
@@ -10,26 +10,38 @@
 
 module.exports = function(grunt) {
   var svgfit = require('svgfit');
+  var async = require("async");
 
   grunt.registerMultiTask('svgfit', 'Renders then saves svg files to their exact bounds', function() {
     var options = this.options({
     });
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      });
+    if( this.files.length === 0 ){
+      grunt.log.writeln( "No files to read!" );
+      return;
+    }
 
-      svgfit.convert(src, f.orig.dest);
-    });
+    var done = this.async();
+
+    async.each(this.files,
+      function(f, callback){
+        var src = f.src.filter(function(filepath) {
+          if (!grunt.file.exists(filepath)) {
+            grunt.log.warn('Source file "' + filepath + '" not found.');
+            return false;
+          } else {
+            return true;
+          }
+        });
+
+        svgfit.convert(src, f.orig.dest).then(function(){
+          callback();
+        });
+      },
+      function(err){
+        done();
+      }
+    );
   });
 
 };
