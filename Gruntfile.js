3c7975dd1cf942c1baae5e49ccf03605ba124601
Mike Mellor <mike.mellor@f-grp.com>
2016-03-13 21:47:15 +0000

    fixed grunt task order

--- Gruntfile.js
+++ Gruntfile.js
@@ -92,13 +92,14 @@ module.exports = function(grunt) {
   grunt.loadTasks('tasks');
 
   // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-bump');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
   // Whenever the "test" task is run, first clean the ".tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'svgfit', 'nodeunit', 'bump']);
+  grunt.registerTask('test', ['clean', 'svgfit', 'nodeunit']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
